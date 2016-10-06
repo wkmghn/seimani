8162f2d46703300d23ebe2f0f39ed2d228b328a9
wkmghn <tailshade@hotmail.com>
2016-10-07 08:31:00 +0900

    [exp-table] ボーナス曜日を更新

--- exp-table.ts
+++ exp-table.ts
@@ -574,7 +574,7 @@ function initializeStageList() {
 
     new StageInfo("H 6-1", 43, 6964, 5370, undefined, undefined, UnitType.Magic),
     new StageInfo("H 6-2", 43, 6956, 5390, undefined, 木, UnitType.Melee),
-    new StageInfo("H 6-3", 43, 6979, 5330, undefined, undefined, UnitType.Ranged),
+    new StageInfo("H 6-3", 43, 6979, 5330, undefined, 金, UnitType.Ranged),
     new StageInfo("H 6-4", 43, 7117, 5360, undefined, undefined, UnitType.Heavy),
     new StageInfo("H 6-5", 44, 7265, 5500, 木, undefined, UnitType.Heavy),
 
